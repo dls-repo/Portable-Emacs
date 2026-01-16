8579f4e5b8cc9299540c0dc86c8b850410ad46e7
Dan La Scala <dlascala@protonmail.com>
2026-01-16 00:52:06 -0800

    revamped with vanilla keybindings

--- Emacs/.emacs.d/config.el
+++ Emacs/.emacs.d/config.el
@@ -3,19 +3,17 @@
       '(("nongnu-devel" . "https://elpa.nongnu.org/nongnu-devel/")
         ("gnu"         . "https://elpa.gnu.org/packages/")
         ("melpa"       . "https://melpa.org/packages/")))
-(package-initialize)
-(package-refresh-contents)
-;; --- Bootstrap use-package ---
+
+(unless package--initialized
+  (package-initialize))
+
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
+  (unless package-archive-contents
+    (package-refresh-contents))
   (package-install 'use-package))
+
 (require 'use-package)
 (setq use-package-always-ensure t)
-
-(use-package beacon
-  :defer 0
-  :config
-  (beacon-mode 1))
 
 (use-package company
   :ensure t
@@ -25,56 +23,59 @@
   (setq company-idle-delay 0.2)  ; Wait 200ms before showing suggestions
   (setq company-minimum-prefix-length 1))  ; Minimum prefix length for suggestions
 
-(global-visual-line-mode 1) ;;allow text wrapping
+(keymap-set global-map "C-c c" #'compile)
+(keymap-set global-map "C-c d" #'duplicate-line)
+(keymap-set global-map "C-c =" #'align-regexp)
+(setq compile-command "cc")
+
+
+;; alias expansion
+(defun my/expand-compile-alias (cmd)
+  (if (string= cmd "cc")
+      (let ((src (shell-quote-argument buffer-file-name))
+            (out (shell-quote-argument
+                  (file-name-sans-extension
+                   (file-name-nondirectory buffer-file-name)))))
+        (format "gcc -Wall -Wextra -Wno-missing-braces -Wno-unused-parameter"
+                out src))
+    cmd))
+
 (electric-indent-mode 1)
-(display-time-mode 1)
+(setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode 1)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (setq org-startup-with-inline-images t)
+(windmove-default-keybindings 'control)
+(winner-mode 1) ;;undo but for window layout
+(global-visual-line-mode 1)
+(load-theme 'deeper-blue t)
+(setq inhibit-startup-screen t)
+(ido-mode 1)
 
-(use-package zenburn-theme
-  :config
-  (load-theme 'zenburn t))
+(use-package expand-region
+  :bind (("C-\"" . er/expand-region)   ;; expand
+         ("C-:"  . er/contract-region))) ;; contract
 
-(setq evil-want-C-u-scroll t)
-(setq evil-want-keybinding nil)
-(require 'evil)
-(when (require 'evil-collection nil t)
-  (evil-collection-init))
+(pending-delete-mode t)
 
-(use-package evil
-  :defer 0
-  :config
-  (evil-mode 1))
-
-;; Optional: make ESC quit prompts everywhere
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-(use-package gptel
+(use-package magit
   :ensure t
-  :commands gptel
-  :config
-  (setq gptel-api-key "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")
-  (setq gptel-default-mode 'org-mode))
+  :bind (("C-x g" . magit-status)))
 
-(use-package ivy
-  :defer 0
-  :config
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t
-        enable-recursive-minibuffers t))
+(use-package multiple-cursors
+  :ensure t
+  :bind
+  (("C-c m c" . mc/edit-lines)        ; Add cursors to each line in region
+   ("C->" . mc/mark-next-like-this) ; Add a cursor to next occurrence
+   ("C-<" . mc/mark-previous-like-this) ; Add cursor to previous occurrence
+   ("C-c m a" . mc/mark-all-like-this))) ; Add cursors to all matches
 
 (use-package org
   :ensure t
   :config
   (require 'org-tempo))
 
-;; Do NOT auto-indent Org src blocks
-(setq org-src-preserve-indentation t
-      org-edit-src-content-indentation 0
-      org-adapt-indentation nil
-      org-src-tab-acts-natively t)
 (setq org-babel-python-command "python3")
 
 (org-babel-do-load-languages
@@ -83,13 +84,57 @@
       (emacs-lisp . t)
       (C . t)))
 
+(with-eval-after-load 'org
+  ;; Temporary output directory for LaTeX previews
+  (setq org-latex-preview-output-directory
+        (let ((dir (expand-file-name "tmp/latex" user-emacs-directory)))
+          (unless (file-exists-p dir) (make-directory dir t))
+          dir))
+  (setq temporary-file-directory org-latex-preview-output-directory)
+
+  ;; Setup dvisvgm process using full paths
+  (setq org-latex-preview-process-alist
+        `((dvisvgm
+           :programs ("latex" "dvisvgm")
+           :description "dvi -> svg"
+           :message "Ensure LaTeX and dvisvgm are installed"
+           :use-xcolor t
+           :image-input-type "dvi"
+           :image-output-type "svg"
+           :image-size-adjust (1.0 . 1.0)
+           :latex-compiler (,latex-exe "-interaction" "nonstopmode"
+                                      "-output-directory" "%o" "%f")
+           :image-converter (,dvisvgm-exe "%f" "-n" "-b" "min" "-o" "%O"))))
+
+  ;; Set default process
+  (setq org-latex-preview-default-process 'dvisvgm)
+
+  ;; Automatically preview LaTeX on startup
+  (setq org-startup-with-latex-preview t))
+
+(defun my/org-preview-toggle ()
+  "Toggle LaTeX previews in Org-mode."
+  (interactive)
+  (require 'org)
+  (org-preview-latex-fragment))
+
+(global-set-key (kbd "C-c C-x C-l") 'my/org-preview-toggle)
+
 (use-package flyspell
   :ensure t
   :init
   (setq ispell-program-name "aspell") ;; or "ispell", depending on your preference
   :hook
-  (text-mode . flyspell-mode)          
-  (prog-mode . flyspell-prog-mode))    
+  ;; Enable flyspell only in text and org modes
+  ((text-mode org-mode) . flyspell-mode)
+  ;; Ensure flyspell ignores code blocks in org-mode
+  :config
+  (defun my/org-mode-flyspell-setup ()
+    "Disable flyspell in Org src blocks."
+    (setq-local flyspell-verify-ignore-predicate
+                (lambda ()
+                  (not (org-in-src-block-p)))))
+  (add-hook 'org-mode-hook #'my/org-mode-flyspell-setup))
 
 (use-package langtool
   :ensure t
@@ -98,8 +143,27 @@
   (setq langtool-language-tool-jar "~/LanguageTool-6.6/languagetool-server.jar") ;; Set the jar path
   (global-set-key (kbd "C-c h") 'langtool-check)) ;; Keybinding for grammar checking
 
-(with-eval-after-load 'evil
-  (define-key evil-normal-state-map (kbd "C-<left>") #'evil-window-left)
-  (define-key evil-normal-state-map (kbd "C-<up>") #'evil-window-up)
-  (define-key evil-normal-state-map (kbd "C-<down>") #'evil-window-down)
-  (define-key evil-normal-state-map (kbd "C-<right>") #'evil-window-right))
+(use-package astyle
+  :ensure t  ;; installs the Emacs Lisp wrapper from MELPA
+  :commands (astyle-buffer astyle-region)
+  :config
+  ;; Example: set default style options
+(setq astyle-options '("--style=kr" "--indent=spaces=4")))
+(defun astyle-buffer ()
+  (interactive)
+  (let ((saved-line-number (line-number-at-pos)))
+    (shell-command-on-region
+     (point-min)
+     (point-max)
+     "astyle --style=kr"
+     nil
+     t)
+    (goto-line saved-line-number)))
+ (global-set-key (kbd "C-c a") 'astyle-buffer)
+
+(use-package yasnippet
+  :ensure t
+  :hook ((prog-mode . yas-minor-mode)
+         (org-src-mode . yas-minor-mode))
+  :config
+  (yas-reload-all))
---
+++ Emacs/Msys2/home/mod/.bash_history
@@ -0,0 +1,14 @@
+pacman -S --needed   mingw-w64-x86_64-toolchain   mingw-w64-x86_64-cmake   mingw-w64-x86_64-libvterm   mingw-w64-x86_64-libtool   mingw-w64-x86_64-make
+pacman -Syu
+pacman -S --needed base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake
+l
+ls
+cd ..
+ls
+cd ..
+ls
+pwd
+cd ..
+ls
+cd ..
+ls
